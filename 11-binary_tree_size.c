f509e2944156a40afba8be7246bf0c50c5070afe
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-21 11:42:21 -0700

    11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size- finds the size of a binary tree
+ * @tree: node to be meausred from
+ * 
+ * Return: the size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
