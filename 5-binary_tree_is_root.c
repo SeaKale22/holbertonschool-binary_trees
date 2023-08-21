63dbab4acd22498bd784d4445754ddd2d20471fb
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-21 07:26:31 -0700

    created binary_tree_is_root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root- cheacks if given node is a root node
+ * @node: Node to be checked
+ *
+ * Return: 1 if root, else 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->parent)
+		return (0);
+	else
+		return (1);
+}
