4496ff11f565748af38506a33e0bc1820e3eb962
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-22 12:54:26 -0700

    15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,8 +11,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (binary_tree_is_full(tree->right) == 0 
-	|| binary_tree_is_full(tree->left) == 0)
+	if (binary_tree_is_full(tree->right) == 0)
+		return (0);
+	if (binary_tree_is_full(tree->left) == 0)
 		return (0);
 	else
 		return (1);
